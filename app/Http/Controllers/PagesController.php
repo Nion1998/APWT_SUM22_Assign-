c169e01d28bd713d82cfe26fba0caf6945829ffe
Nion1998 <55551279+Nion1998@users.noreply.github.com>
2022-06-12 02:38:09 +0600

    validation rouls create

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,10 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rules\Password;
 
 class PagesController extends Controller
 {
-    function home(){
-        return view("public.home");
+    function home()
+    {
+        return view('Pages.home');
+    }
+    function register()
+    {
+        return view('pages.register');
+    }
+
+    function submit(Request $requst)
+    {
+        $rules = [
+            "name"=>"required|max:20|min:5",
+                "email"=>"required|email|unique:users,email",
+                'password' =>[
+                    'required',
+                    Password::min(8)->letters()->numbers()->mixedCase()->symbols()
+                ],
+                "conf_password"=>"same:password"
+        ];
+        
+        $messages=[
+            "required"=>"Please fillup this field",
+            "name.max"=>"Name should not exceed 10 characters",
+            'conf_password.same'=> "Password & confirm password does not match",
+        ];
+
+        $this->validate($requst ,$rules,$messages);
+        
+        
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route ::get('/',[PagesController::class,'home'])->name('home');
+Route::get('/',[PagesController::class,'home'])->name('pages.home');
+Route::get('/register',[PagesController::class,'register'])->name('pages.register');
+Route::post('/register',[PagesController::class,'submit'])->name('pages.submit');
